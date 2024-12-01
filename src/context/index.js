b1d0abcb1c44cf4126ec35d93c83b992f343546d
Stefano Marra <stefano.marra1987@gmail.com>
2024-12-02 00:10:34 +0100

    Add ExtensionContextProvider

---
+++ src/context/index.js
@@ -0,0 +1,59 @@
+import React, {
+  createContext,
+  useContext,
+  useState,
+  useEffect,
+} from 'react'
+
+/**
+ * Default context object
+ */
+const defaultContext = {
+  userContext: undefined,
+  environment: {
+    viewport: undefined,
+  },
+}
+
+const ExtensionContext = createContext(defaultContext)
+
+/**
+ * Extension Context Provider
+ */
+export const ExtensionContextProvider = ({ children }) => {
+  const [context, setContext] = useState(defaultContext);
+
+  /**
+   * When teachfloor SDK is initialized:
+   * - Start listening to event "__set_context"
+   * - Update context state value
+   */
+  useEffect(() => {
+    if (tf) {
+      tf('onInit', (API) => {
+        API.on('__set_context', (context) => {
+          setContext(context)
+        })
+      })
+    }
+  }, [])
+
+  return (
+    <ExtensionContext.Provider value={context}>
+      {children}
+    </ExtensionContext.Provider>
+  );
+}
+
+/**
+ * Extension context hook
+ */
+export const useExtensionContext = () => {
+  const context = useContext(ExtensionContext);
+
+  if (!context) {
+    throw new Error('useExtensionContext must be used within ExtensionContextProvider');
+  }
+
+  return context;
+};
